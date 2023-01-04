615cb7d39655152f9f810713cf51084a3949dbdb
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-01-03 21:35:56 -0500

    Completed day 1

---
+++ src/day2.rs
@@ -0,0 +1,16 @@
+#[aoc_generator(day2)] 
+pub fn input_generator(input: &str) -> Vec<u32> {
+}
+
+#[aoc(day2, part1)] 
+pub fn solver_part1(input: &Vec<u32>) -> u32 {
+}
+
+#[aoc(day2, part2)] 
+pub fn solver_part2(input: &Vec<u32>) -> u32 {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
