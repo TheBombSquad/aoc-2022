820025c4ba86132605986c55260e7adef3b85869
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-01-07 21:56:24 -0500

    Completed day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,24 +1,31 @@
 use std::collections::HashSet;
 
+fn get_priority(character: u8) -> u8 {
+    if character > 97 {
+        return character - 96;
+    } else {
+        return character - 38;
+    }
+}
+
 pub struct Rucksack {
     compartment_a: String,
     compartment_b: String,
 }
-
 impl Rucksack {
-    fn get_priority(character: u8) -> u8 {
-        if character > 97 {
-            return character - 96
-        }
-        else {
-            return character - 38
-        }
-    }
     fn get_shared(&self) -> u8 {
         let a_set: HashSet<u8> = self.compartment_a.as_bytes().iter().cloned().collect();
         let b_set: HashSet<u8> = self.compartment_b.as_bytes().iter().cloned().collect();
         return *a_set.intersection(&b_set).collect::<Vec<&u8>>()[0];
     }
+}
+
+fn get_group_shared(chunk: &[String]) -> u8 {
+    let a_set: HashSet<u8> = chunk[0].as_bytes().iter().cloned().collect();
+    let b_set: HashSet<u8> = chunk[1].as_bytes().iter().cloned().collect();
+    let c_set: HashSet<u8> = chunk[2].as_bytes().iter().cloned().collect();
+    let a_intersect_b = a_set.intersection(&b_set).cloned().collect::<HashSet<u8>>();
+    *a_intersect_b.intersection(&c_set).collect::<Vec<&u8>>()[0]
 }
 
 #[aoc_generator(day3)]
@@ -38,16 +45,22 @@ pub fn input_generator(input: &str) -> Vec<Rucksack> {
 pub fn solver_part1(input: &[Rucksack]) -> u32 {
     input
         .into_iter()
-        .map(|x| Rucksack::get_priority(x.get_shared()))
+        .map(|x| get_priority(x.get_shared()))
         .map(|x| u32::from(x))
         .sum()
 }
 
-/*
 #[aoc(day3, part2)]
-pub fn solver_part2(input: &[]) -> () {
-
-}*/
+pub fn solver_part2(input: &[Rucksack]) -> u32 {
+    input
+        .into_iter()
+        .map(|x| x.compartment_a.clone() + &x.compartment_b)
+        .collect::<Vec<String>>()
+        .chunks(3)
+        .map(|x| get_priority(get_group_shared(x)))
+        .map(|x| u32::from(x))
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
