e05ada47381c0808086782680142df4523e54451
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-01-08 19:48:47 -0500

    Improved day 3 - it's faster without using HashSet

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 fn get_priority(character: u8) -> u8 {
     if character > 97 {
         return character - 96;
@@ -9,23 +7,34 @@ fn get_priority(character: u8) -> u8 {
 }
 
 pub struct Rucksack {
-    compartment_a: String,
-    compartment_b: String,
+    side_a: String,
+    side_b: String,
 }
 impl Rucksack {
     fn get_shared(&self) -> u8 {
-        let a_set: HashSet<u8> = self.compartment_a.as_bytes().iter().cloned().collect();
-        let b_set: HashSet<u8> = self.compartment_b.as_bytes().iter().cloned().collect();
-        return *a_set.intersection(&b_set).collect::<Vec<&u8>>()[0];
+        let a_collection = self.side_a.as_bytes();
+        let intersection: Vec<&u8> = self.side_b
+            .as_bytes()
+            .iter()
+            .filter(|x| a_collection.contains(x))
+            .collect();
+        *intersection[0]
     }
 }
 
 fn get_group_shared(chunk: &[String]) -> u8 {
-    let a_set: HashSet<u8> = chunk[0].as_bytes().iter().cloned().collect();
-    let b_set: HashSet<u8> = chunk[1].as_bytes().iter().cloned().collect();
-    let c_set: HashSet<u8> = chunk[2].as_bytes().iter().cloned().collect();
-    let a_intersect_b = a_set.intersection(&b_set).cloned().collect::<HashSet<u8>>();
-    *a_intersect_b.intersection(&c_set).collect::<Vec<&u8>>()[0]
+    let a_collection = chunk[0].as_bytes();
+    let intersection_a_b: Vec<&u8> = chunk[1]
+        .as_bytes()
+        .iter()
+        .filter(|x| a_collection.contains(x))
+        .collect();
+    let intersection_a_b_c: Vec<&u8> = chunk[2]
+        .as_bytes()
+        .iter()
+        .filter(|x| intersection_a_b.contains(x))
+        .collect();
+    *intersection_a_b_c[0]
 }
 
 #[aoc_generator(day3)]
@@ -34,8 +43,8 @@ pub fn input_generator(input: &str) -> Vec<Rucksack> {
         .split_whitespace()
         .map(|x| x.split_at(x.len() / 2))
         .map(|x| Rucksack {
-            compartment_a: x.0.to_owned(),
-            compartment_b: x.1.to_owned(),
+            side_a: x.0.to_owned(),
+            side_b: x.1.to_owned(),
         })
         .collect();
     rucksack_split
@@ -54,7 +63,7 @@ pub fn solver_part1(input: &[Rucksack]) -> u32 {
 pub fn solver_part2(input: &[Rucksack]) -> u32 {
     input
         .into_iter()
-        .map(|x| x.compartment_a.clone() + &x.compartment_b)
+        .map(|x| x.side_a.clone() + &x.side_b)
         .collect::<Vec<String>>()
         .chunks(3)
         .map(|x| get_priority(get_group_shared(x)))
